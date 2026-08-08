f09171f39b46bc7b59fb0bcf51057b0459472682
heyvivektiwari <heyvivektiwari@gmail.com>
2026-08-08 14:42:31 +0530

    chore: Remove StudentPassModal section from dashboard header

--- components/dashboard-header.tsx
+++ components/dashboard-header.tsx
@@ -9,7 +9,6 @@ import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@
 import { SEMESTER_MONTHS } from "@/lib/attendance-store"
 import { Logo } from "@/components/logo"
 import { ExportReportModal } from "@/components/export-report-modal"
-import { StudentPassModal } from "@/components/student-pass-modal"
 import { cn } from "@/lib/utils"
 import { Capacitor } from '@capacitor/core'
 import { Preferences } from '@capacitor/preferences'
@@ -316,7 +315,6 @@ export function DashboardHeader() {
           )}
 
           <ExportReportModal />
-          <StudentPassModal />
           <ModeToggle />
         </div>
       </div>
